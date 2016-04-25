8e0335439cf0b46eb06ecb52045a16d090aad017
goodhamgupta <shubhamg2208@live.com>
2016-04-25 23:15:19 +0530

    Removed class based views. basic login complete

--- github_scraper/urls.py
+++ github_scraper/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from scraper.views import *
-router = DefaultRouter()
-router.register(r'scraper', ScraperViewSet, 'scraperviewset')
-router.register(r'login',LoginViewSet,'loginviewset')
 
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r'scraper', ScraperViewSet, 'scraperviewset')
+# router.register(r'login',LoginViewSet,'loginviewset')
+#
+# urlpatterns = router.urls
+from scraper import views
 
-
+urlpatterns = [
+    # ex: /polls/
+    url(r'^login/$', views.login, name='index'),
+    # ex: /polls/5/
+    ]
 
--- scraper/views.py
+++ scraper/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authentication import TokenAuthentication
@@ -16,45 +17,53 @@ from datetime import datetime,timedelta
 
 
 
-class ScraperViewSet(viewsets.ViewSet):
+#class ScraperViewSet(viewsets.ViewSet):
 
-    def list(self,request):
-        headers = {"Accept": "application/vnd.github.v3+json"}
-        url = "https://www.github.com/saltstack/salt"
+def list(request):
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    url = "https://www.github.com/saltstack/salt"
 
-        #repo_name = re.search('.github.com/(.+?)/*$',url).group()
-        user_repo = url.split("github.com/",1)[1]
-        user = user_repo.split("/",2)[0]
-        repo = user_repo.split("/",2)[1]
+    #repo_name = re.search('.github.com/(.+?)/*$',url).group()
+    user_repo = url.split("github.com/",1)[1]
+    user = user_repo.split("/",2)[0]
+    repo = user_repo.split("/",2)[1]
 
-        r = requests.get("https://api.github.com/repos/"+str(user)+"/"+str(repo),headers=headers)
+    r = requests.get("https://api.github.com/repos/"+str(user)+"/"+str(repo),headers=headers)
 
-        data = r.json()
-        flag=0
-        if r.status_code == 200:
-            logging.info("Successful")
-            for key,value in data.iteritems():
-                    if key == "open_issues_count":
-                        issues_open = value
-
-
-        one_day_back = datetime.now()-timedelta(hours=24)
-        issues_24_url = "https://api.github.com/repos/"+str(user)+"/"+str(repo)+"/issues?since="+str(one_day_back)+"&per_page=10000"
-        print issues_24_url
-        r = requests.get(issues_24_url)
-        issues_day_count = len(r.json())
-
-        one_week_back = datetime.now()-timedelta(hours=168)
-        one_week_url = issues_24_url = "https://api.github.com/repos/"+str(user)+"/"+str(repo)+"/issues?since="+str(one_week_back)+"&per_page=10000"
-        r = requests.get(one_week_url,headers=headers)
-        issues_week_count = len(r.json())
-        return Response({"status":r.status_code,"data":{"open issues":issues_open,"1 day old":issues_day_count,"week old":issues_week_count,"Older":issues_open-issues_week_count}})
+    data = r.json()
+    flag=0
+    if r.status_code == 200:
+        logging.info("Successful")
+        for key,value in data.iteritems():
+                if key == "open_issues_count":
+                    issues_open = value
 
 
-class LoginViewSet(viewsets.ViewSet):
+    one_day_back = datetime.now()-timedelta(hours=24)
+    issues_24_url = "https://api.github.com/repos/"+str(user)+"/"+str(repo)+"/issues?since="+str(one_day_back)+"&per_page=10000"
+    print issues_24_url
+    r = requests.get(issues_24_url)
+    issues_day_count = len(r.json())
 
-    def list(self,request):
+    one_week_back = datetime.now()-timedelta(hours=168)
+    one_week_url = issues_24_url = "https://api.github.com/repos/"+str(user)+"/"+str(repo)+"/issues?since="+str(one_week_back)+"&per_page=10000"
+    r = requests.get(one_week_url,headers=headers)
+    issues_week_count = len(r.json())
+    return Response({"status":r.status_code,"data":{"open issues":issues_open,"1 day old":issues_day_count,"week old":issues_week_count,"Older":issues_open-issues_week_count}})
+
+
+#class LoginViewSet(viewsets.ViewSet):
+@api_view(['GET','POST'])
+def login(request):
+    if request.method == "GET":
         return render(request,"login.html")
-
-    def post(self,request):
-        print request.data
+    if request.method == "POST":
+        '''
+        Perform authentication here
+        '''
+        x = list(request)
+        return x
+def post(request):
+    print "Hello"
+    print request.body
+    print request.POST
