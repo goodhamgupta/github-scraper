b40204936f761440d7d6a221447edbf83a1e2c82
goodhamgupta <shubhamg2208@live.com>
2016-04-26 19:24:18 +0530

    Experiment with static dir for heroku

--- github_scraper/settings.py
+++ github_scraper/settings.py
@@ -53,12 +53,23 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'github_scraper.urls'
-PROJECT_DIR=os.path.dirname(__file__)
-STATIC_ROOT= os.path.join(PROJECT_DIR,'static')
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+# PROJECT_DIR=os.path.dirname(__file__)
+# STATIC_ROOT= os.path.join(PROJECT_DIR,'static')
+# PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 TEMPLATE_DIRS = (
-    PROJECT_PATH + '/templates/',
+    PROJECT_ROOT + '/templates/',
 )
 
 TEMPLATES = [
